b859f7548ad7231fabca11486f25a7dd1d837d3c
Shubh Arya <56305668+shubh-learner@users.noreply.github.com>
2020-07-01 05:41:26 +0530

    updated

---
+++ README.md
@@ -0,0 +1,7 @@
+# Emotion_Detection
+
+
+
+References:
+https://www.auduno.com/clmtrackr/docs/reference.html
+https://observablehq.com/@d3/bar-chart-race-explained
---
+++ src/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
+
+app.set('view engine', 'html');
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/services', express.static(__dirname + '/services'));
+app.use('/css', express.static(__dirname + '/css'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname , './index.html'));
+});
+
+server.listen(process.env.PORT || 5000);
+
